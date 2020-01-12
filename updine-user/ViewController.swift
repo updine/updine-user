e1c8ee8da708d3c1e6f4ee1c65d3abd34aa048c7
yasinehsan <yasinehsan11@gmail.com>
2020-01-12 15:23:22 +0400

    added boiler

---
+++ updine-user/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  updine-user
+//
+//  Created by Yasin Ehsan on 1/12/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
