72423e3578e7595fd76c2feb163b082e7059cb3b
yasinehsan <yasinehsan11@gmail.com>
2020-01-14 19:46:43 +0600

    added pods + git files

---
+++ updine-user/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  updine-user
+//
+//  Created by Yasin Ehsan on 1/14/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+}
