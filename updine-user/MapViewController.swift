01a93359cf780d9b3f246481be02026dff5bb5fd
yasinehsan <yasinehsan11@gmail.com>
2020-01-15 07:54:35 +0600

    added location fcode

--- updine-user/MapViewController.swift
+++ updine-user/MapViewController.swift
@@ -9,11 +9,81 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var mapView: MKMapView!
     
-    }
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    
+    
+  override func viewDidLoad() {
+          super.viewDidLoad()
+          checkLocationServices()
+      }
+      
+      
+      func setupLocationManager() {
+          locationManager.delegate = self
+          locationManager.desiredAccuracy = kCLLocationAccuracyBest
+      }
+      
+      
+      func centerViewOnUserLocation() {
+          if let location = locationManager.location?.coordinate {
+              let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+              mapView.setRegion(region, animated: true)
+          }
+      }
+      
+      
+      func checkLocationServices() {
+          if CLLocationManager.locationServicesEnabled() {
+              setupLocationManager()
+              checkLocationAuthorization()
+          } else {
+              // Show alert letting the user know they have to turn this on.
+          }
+      }
+      
+      
+      func checkLocationAuthorization() {
+          switch CLLocationManager.authorizationStatus() {
+          case .authorizedWhenInUse:
+              mapView.showsUserLocation = true
+              centerViewOnUserLocation()
+              locationManager.startUpdatingLocation()
+              break
+          case .denied:
+              // Show alert instructing them how to turn on permissions
+              break
+          case .notDetermined:
+              locationManager.requestWhenInUseAuthorization()
+          case .restricted:
+              // Show an alert letting them know what's up
+              break
+          case .authorizedAlways:
+              break
+          @unknown default:
+            print("fatal erroe unkown in case chk location")
+        }
+      }
+  }
 
-}
+
+  extension MapsViewController: CLLocationManagerDelegate {
+      
+      func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+          guard let location = locations.last else { return }
+          let region = MKCoordinateRegion.init(center: location.coordinate, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+          mapView.setRegion(region, animated: true)
+      }
+      
+      
+      func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+          checkLocationAuthorization()
+      }
+  }
+
+
+
