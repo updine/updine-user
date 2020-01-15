4ef2ff39f7c1f5326cbc8f197b4eb8673a1d0e95
yasinehsan <yasinehsan11@gmail.com>
2020-01-15 10:58:00 +0600

    added calendar event

--- updine-user/DineDetailsViewController.swift
+++ updine-user/DineDetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EventKit
 
 class DineDetailsViewController: UIViewController {
 
@@ -16,7 +17,35 @@ class DineDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func calenderButtonTapped(_ sender: Any) {
+        
+        let eventStore:EKEventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: .event) {(granted, error) in
+            if (granted) && (error) == nil
+            {
+                print("granted \(granted)")
+                print("error \(error)")
+                
+                let event:EKEvent = EKEvent(eventStore: eventStore)
+                event.title = "Hurricane Dorian Clothing Drive"
+                event.startDate = Date()
+                event.endDate = Date()
+                event.notes = ""
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                } catch let error as NSError{
+                    print("error : \(error)")
+                }
+                print("Save Event")
+            } else{
+                print("error : \(error)")
+            }
+            
+        }
+    }
+    
    
 
 }
