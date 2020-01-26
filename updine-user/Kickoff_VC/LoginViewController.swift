2b785a3039d2349c8de48c6543819365e71bc66b
yasinehsan <yasinehsan11@gmail.com>
2020-01-26 03:57:55 -0500

    chnaed reg vc to homie vc

---
+++ updine-user/Kickoff_VC/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  updine-user
+//
+//  Created by Yasin Ehsan on 1/26/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "popOutNoteExpanded" {
+//            let vc = segue.destination as! ExpandedCellViewController
+//            let cell = sender as! AnnotatedPhotoCell
+//            sourceCell = cell
+//            vc.picture = cell.imageView.image
+//            print("button pressed")
+//        }
+//    }
+
+}
